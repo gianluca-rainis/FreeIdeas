1544b2d68546ba0e2e7a8ceb6704b59875b4d2b1
Gianluca Rainis <gianluca.rainis@isisbem.it>
2025-07-19 11:18:56 +0200

    Fixed the og data.

--- scripts/commonParts.js
+++ scripts/commonParts.js
@@ -105,23 +105,23 @@ function loadMetadata() {
             
             <meta name="description" content="Free Ideas is a site where you can publish your ideas for projects, apps, and websites, and where you can find inspiration for your next project." />
             
-            <meta property="og:url" content="https://www.freeideas.duckdns.org/" />
+            <meta property="og:url" content="https://freeideas.duckdns.org/" />
             <meta property="og:type" content="website" />
             <meta property="og:site_name" content="FreeIdeas" />
             <meta property="og:title" content="FreeIdeas" />
             <meta property="og:description" content="Free Ideas is a site where you can publish your ideas for projects, apps, and websites, and where you can find inspiration for your next project." />
-            <meta property="og:image" content="https://www.freeideas.duckdns.org/images/freeideasPreview.png" />
-            <meta property="og:image:secure_url " content="https://www.freeideas.duckdns.org/images/freeideasPreview.png" />
+            <meta property="og:image" content="https://freeideas.duckdns.org/images/freeideasPreview.png" />
+            <meta property="og:image:secure_url " content="https://freeideas.duckdns.org/images/freeideasPreview.png" />
             <meta property="og:image:type" content="image/png" />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
             
             <meta name="twitter:card" content="summary_large_image" />
             <meta property="twitter:domain" content="freeideas.duckdns.org">
-            <meta name="twitter:url" content="https://www.freeideas.duckdns.org/" />
+            <meta name="twitter:url" content="https://freeideas.duckdns.org/" />
             <meta name="twitter:title" content="FreeIdeas" />
             <meta name="twitter:description" content="Free Ideas is a site where you can publish your ideas for projects, apps, and websites, and where you can find inspiration for your next project." />
-            <meta name="twitter:image" content="https://www.freeideas.duckdns.org/images/freeideasPreview.png" />
+            <meta name="twitter:image" content="https://freeideas.duckdns.org/images/freeideasPreview.png" />
 
             <link rel="icon" href="./images/FreeIdeas.svg" type="image/x-ico" />
             <link href="./styles/styles.css" rel="stylesheet" />
