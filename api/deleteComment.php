099111717cc9e752941423b7478c4bf67cd098fc
gianluca-rainis <gianlucarainis@gmail.com>
2025-09-23 14:58:29 +0200

    Changed the delete comment logic, now when you delete a comment without subcomments it'll be deleted, if you delete a comment with subcomments it will be changed the text to the "This comment was deleted by the author" and the subcomments will remain. If you delete a subcomment of a comment that was 'deleted' and it haven't other subcomments they'll be removed both.

--- api/deleteComment.php
+++ api/deleteComment.php
@@ -6,6 +6,7 @@
     session_start();
 
     $id = "";
+    $authorDeletedCommentid = 14; // Need an account named "Deleted" without profile image with id 14 to work
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id = getInput($_POST["id"]);
@@ -17,34 +18,39 @@
     }
 
     // Delete the comment
-    try {
-        $sql = "UPDATE comments SET authorid=?, description=? WHERE id=?;";
-        $state = $conn->prepare($sql);
+    if (!controlIfHaveSubcomments($conn, $id)) {
+        deleteCommentAndSuperCommentsWithoutSubComments($conn, $id);
+    }
+    else { // Delete the info about the comment without delete the subcomments
+        try {
+            $sql = "UPDATE comments SET authorid=?, description=? WHERE id=?;";
+            $state = $conn->prepare($sql);
 
-        if (!$state) {
-            echo json_encode(["success"=>false, "error"=>"database_connection"]);
+            if (!$state) {
+                echo json_encode(["success"=>false, "error"=>"database_connection"]);
+                exit;
+            }
+
+            $description = "This comment was deleted by the author.";
+
+            $state->bind_param("isi", $authorDeletedCommentid, $description, $id);
+            
+            if (!$state->execute()) {
+                echo json_encode(["success"=>false, "error"=>"execution_command_delete_comment"]);
+                exit;
+            }
+
+            $state->close();
+        } catch (\Throwable $th) {
+            echo json_encode(["success"=>false, "error"=>strval($th)]);
             exit;
         }
-
-        $authorid = 14; // Need an account named "Deleted" without profile image with id 14 to work
-        $description = "This comment was deleted by the author.";
-
-        $state->bind_param("ssi", $authorid, $description, $id);
-        
-        if (!$state->execute()) {
-            echo json_encode(["success"=>false, "error"=>"execution_command_delete_comment"]);
-            exit;
-        }
-
-        $state->close();
-    } catch (\Throwable $th) {
-        echo json_encode(["success"=>false, "error"=>strval($th)]);
-        exit;
     }
 
     $conn->close();
 
     echo json_encode(["success"=>true]);
+    exit;
 
     function getInput($data) {
         $data = trim($data);
@@ -52,6 +58,149 @@
         $data = htmlspecialchars($data);
 
         return $data;
+    }
+
+    // Control if the comment have subcomments
+    function controlIfHaveSubcomments($conn, $id) {
+        $ret = false;
+
+        try {
+            $sql = "SELECT * FROM comments WHERE superCommentid=?;";
+            $state = $conn->prepare($sql);
+
+            if (!$state) {
+                echo json_encode(["success"=>false, "error"=>"database_connection"]);
+                exit;
+            }
+
+            $state->bind_param("i", $id);
+            
+            if (!$state->execute()) {
+                echo json_encode(["success"=>false, "error"=>"execution_command_get_subcomments"]);
+                exit;
+            }
+
+            $result = $state->get_result();
+
+            if ($result->num_rows > 0) {
+                $ret = true;
+            }
+
+            $state->close();
+        } catch (\Throwable $th) {
+            echo json_encode(["success"=>false, "error"=>strval($th)]);
+            exit;
+        }
+
+        return $ret;
+    }
+
+    // Get the superCommentid
+    function getSuperCommentid($conn, $id) {
+        $ret = null;
+
+        try {
+            $sql = "SELECT superCommentid FROM comments WHERE id=?;";
+            $state = $conn->prepare($sql);
+
+            if (!$state) {
+                echo json_encode(["success"=>false, "error"=>"database_connection"]);
+                exit;
+            }
+
+            $state->bind_param("i", $id);
+            
+            if (!$state->execute()) {
+                echo json_encode(["success"=>false, "error"=>"execution_command_get_subcomments"]);
+                exit;
+            }
+
+            $result = $state->get_result();
+
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+
+                $ret = $row['superCommentid'];
+            }
+
+            $state->close();
+        } catch (\Throwable $th) {
+            echo json_encode(["success"=>false, "error"=>strval($th)]);
+            exit;
+        }
+
+        return $ret;
+    }
+
+    // Get the authorid of the comment
+    function getAuthorId($conn, $id) {
+        $ret = null;
+
+        try {
+            $sql = "SELECT authorid FROM comments WHERE id=?;";
+            $state = $conn->prepare($sql);
+
+            if (!$state) {
+                echo json_encode(["success"=>false, "error"=>"database_connection"]);
+                exit;
+            }
+
+            $state->bind_param("i", $id);
+            
+            if (!$state->execute()) {
+                echo json_encode(["success"=>false, "error"=>"execution_command_get_subcomments"]);
+                exit;
+            }
+
+            $result = $state->get_result();
+
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+
+                $ret = $row['authorid'];
+            }
+
+            $state->close();
+        } catch (\Throwable $th) {
+            echo json_encode(["success"=>false, "error"=>strval($th)]);
+            exit;
+        }
+
+        return $ret;
+    }
+
+    // Delete the comment and all the supersomments without comment
+    function deleteCommentAndSuperCommentsWithoutSubComments($conn, $id) {
+        global $authorDeletedCommentid;
+
+        $superCommentId = getSuperCommentid($conn, $id);
+        $authorId = getAuthorId($conn, $superCommentId);
+
+        try {
+            $sql = "DELETE FROM comments WHERE id=?;";
+            $state = $conn->prepare($sql);
+
+            if (!$state) {
+                echo json_encode(["success"=>false, "error"=>"database_connection"]);
+                exit;
+            }
+
+            $state->bind_param("i", $id);
+            
+            if (!$state->execute()) {
+                echo json_encode(["success"=>false, "error"=>"execution_command_delete_comment"]);
+                exit;
+            }
+
+            $state->close();
+        } catch (\Throwable $th) {
+            echo json_encode(["success"=>false, "error"=>strval($th)]);
+            exit;
+        }
+
+        if ($superCommentId !== null && !controlIfHaveSubcomments($conn, $superCommentId) && $authorId === $authorDeletedCommentid) {
+            deleteCommentAndSuperCommentsWithoutSubComments($conn, $superCommentId);
+        }
     }
 
     exit;
