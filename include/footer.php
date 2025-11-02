ceeeb5bfb5e0acfcdf233a49e755ebfbdcc13024
gianluca-rainis <gianlucarainis@gmail.com>
2025-11-02 16:48:52 +0100

    Changed the footer style.

--- include/footer.php
+++ include/footer.php
@@ -1,24 +1,40 @@
 <!-- 
     Footer iniected by /include/footer.php
 -->
-
-<ul>
-    <li><a href="./privacyPolicy.php">Privacy Policy</a></li>
-    <li><a href="./termsOfUse.php">Terms of Use</a></li>
-    <li><a href="./sitemap.xml">Sitemap</a></li>
-    <li><a href="./license_info.php">License</a></li>
-    <li><a href="./faq.php">FAQ</a></li>
-    <li><a href="./images/logo/FreeIdeas_official_logos.zip" download="">Official logos</a></li>
-    <li><a href="./contacts.php">Contact Us</a></li>
-    <li><a href="./about.php">About Us</a></li>
-    <li><a href="./reservedArea.php">Reserved Area</a></li>
-    <li><a href="https://github.com/gianluca-rainis/FreeIdeas.git"><img src="./images/github.svg" alt="GitHub Logo" id="githubLogo"></a></li>
-</ul>
-<hr>
 <section class="footerTextSection">
     <a href="./index.php" id="footerLogoA"><img src="./images/FreeIdeas.svg" alt="FreeIdeas Logo" id="footerLogo"></a>
 
     <ul class="footerText">
         <li>Copyright &copy; 2025 Gianluca Rainis</li>
+        <li>freeideas_site@gmail.com</li>
     </ul>
+</section>
+<section class="footerLinksSection">
+    <div>
+        <h4>Legal</h4>
+        <ul>
+            <li><a href="./privacyPolicy.php">Privacy Policy</a></li>
+            <li><a href="./termsOfUse.php">Terms of Use</a></li>
+            <li><a href="./license_info.php">License</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <h4>Support</h4>
+        <ul>
+            <li><a href="./faq.php">FAQ</a></li>
+            <li><a href="./contacts.php">Contact Us</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <h4>More</h4>
+        <ul>
+            <li><a href="./sitemap.xml">Sitemap</a></li>
+            <li><a href="./images/logo/FreeIdeas_official_logos.zip" download="">Official logos</a></li>
+            <li><a href="./about.php">About Us</a></li>
+            <li><a href="./reservedArea.php">Reserved Area</a></li>
+            <li><a href="https://github.com/gianluca-rainis/FreeIdeas.git"><img src="./images/github.svg" alt="GitHub Logo" id="githubLogo"></a></li>
+        </ul>
+    </div>
 </section>
