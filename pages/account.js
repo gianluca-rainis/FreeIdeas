cbf821806068467c0f85cf2f5e088da0d844ee47
gianluca-rainis <gianlucarainis@gmail.com>
2026-02-10 12:21:19 +0100

    Fixed some bugs.

--- pages/account.js
+++ pages/account.js
@@ -28,7 +28,7 @@ export async function getServerSideProps(context) {
             const res = await fetchWithTimeout(`${baseUrl}/api/getSessionData?data=account`, {
                 method: 'GET',
                 headers: cookieHeader ? { cookie: cookieHeader } : {}
-            }, 1000);
+            }, 2000);
 
             const data = await res.json();
 
@@ -50,7 +50,7 @@ export async function getServerSideProps(context) {
             method: "POST",
             headers: cookieHeader ? { cookie: cookieHeader } : {},
             body: formData
-        }, 1000);
+        }, 2000);
 
         const data = await response.json();
         
